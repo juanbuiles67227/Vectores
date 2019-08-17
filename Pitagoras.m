4a031545758c919a3ee98ef68c7f7220d6935bd5
juanbuiles67227 <31153055+juanbuiles67227@users.noreply.github.com>
2019-08-17 15:47:21 -0500

    Rotación de figuras

---
+++ Pitagoras.m
@@ -0,0 +1,19 @@
+A=[0 2;0 3]
+    plot(A(1,:),A(2,:))
+whitebg([0.8 0.9 0.7])
+sc=5
+
+    hold on
+    axis ([-sc sc -sc sc])
+
+p=*pi;
+for i=1:100
+
+    M=[cos(p*i/100) -sin(p*i/100);...
+        sin(p*i/100) cos(p*i/100)]  %Ecuación Canonica, rota el vestor dpendido de pi
+
+    v=M*A;
+    pause(.01)
+    plot(v(1,:),v(2,:))
+    axis ([-sc sc -sc sc])
+end
